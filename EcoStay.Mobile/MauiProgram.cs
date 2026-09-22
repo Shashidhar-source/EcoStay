c88698c17ffb816c7519d49cdd667a18562809ab
shashidhar <shashidhar@gmail.com>
2026-09-22 14:04:46 +0530

    fix(mobile): resolve Android fast-deployment startup crash, embed assemblies into APK, and configure native resources

--- EcoStay.Mobile/MauiProgram.cs
+++ EcoStay.Mobile/MauiProgram.cs
@@ -11,12 +11,7 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            .UseMauiApp<App>();
 
 #if DEBUG
         builder.Logging.AddDebug();
--- EcoStay.Mobile/Platforms/Android/MainApplication.cs
+++ EcoStay.Mobile/Platforms/Android/MainApplication.cs
@@ -9,6 +9,22 @@ public class MainApplication : MauiApplication
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
+		AndroidEnvironment.UnhandledExceptionRaiser += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"[AndroidEnvironment FATAL] {args.Exception}");
+			args.Handled = true;
+		};
+
+		AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"[AppDomain Unhandled] {args.ExceptionObject}");
+		};
+
+		TaskScheduler.UnobservedTaskException += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"[TaskScheduler Unobserved] {args.Exception}");
+			args.SetObserved();
+		};
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
